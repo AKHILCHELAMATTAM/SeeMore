d8a5fa70683fd2d612f4c3887b499e0ad02b8159
AKHILCHELAMATTAM <115454282+AKHILCHELAMATTAM@users.noreply.github.com>
2023-10-13 15:37:33 +0530

    Add files via upload

---
+++ hide&show/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+    $('#btn').click(function(){
+        $('.div').toggle();
+    });
+});
+
+// script.js
+// const button = document.getElementById('color-change-button');
+
+// button.addEventListener('click', () => {
+//     // Generate a random color
+//     const randomColor = `#${Math.floor(Math.random()*16777215).toString(16)}`;
+    
+//     // Change the button's background color
+//     button.style.backgroundColor = randomColor;
+// });
